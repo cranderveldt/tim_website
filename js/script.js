de919f28ebf2d61ec0666e078add42b9dbd39efa
cranderveldt <djnaphouse@gmail.com>
2015-02-01 13:25:25 -0500

    initial files

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var app = angular.module('timsite', []);
+app.controller('Main',['$scope', function ($scope) {
+  
+}]);
+// app.directive('tlRotate', function () {
+//   return {
+//     replace: false,
+//     transclude: false,
+//     restrict: 'A',
+//     scope: false,
+//     link: function ($scope, element, attrs) {
+      
+//     }
+//   };
+// });
